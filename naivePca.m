a0e97a8d6c61b7bba3f47cb78e8012995cb4cb48
ashwinkumarm <ashbushster@gmail.com>
2018-06-03 15:26:45 -0500

    Add files via upload

--- naivePca.m
+++ naivePca.m
@@ -28,34 +28,37 @@ end
 
 function sampleData(EV, trainData, testData)
 for k = [1,2,3,4,5,6,7,8,9,10]
-V=EV(:,1:k);
-[a,b]=size(V);
-piF=zeros(a,1);
-for i =1:a
-    total=0;
-    for j=1:b
-        total=total+(V(i,j)^2);
+    V=EV(:,1:k);
+    [a,b]=size(V);
+    piF=zeros(a,1);
+    for i =1:a
+        total=0;
+        for j=1:b
+            total=total+(V(i,j)^2);
+        end
+        piF(i,1)=total/k;
     end
-    piF(i,1)=total/k;
-end
-for s = 1:20
-    acc = 0;
-for iter = 1 : 100
-samIndex = unique(randsample(57,s,true,piF ));
-train_data = construct(samIndex, trainData);
-test_data = construct(samIndex, testData);
-[o,pr,m,st] = meanAndVar(train_data);
-a = accuracy(test_data,o,pr,m,st);
-acc = acc + a;
-end
-accAvg = (acc/100);
-loss = 100 - accAvg;
-disp("K " + k + " S " + s+ " Accuracy is "+ accAvg+ " loss is " + loss);
-end
+    for s = 1:20
+        acc = 0;
+        for iter = 1 : 100
+            samIndex = unique(randsample(57,s,true,piF ));
+            disp(samIndex);
+            train_data = construct(samIndex, trainData);
+            test_data = construct(samIndex, testData);
+            [o,pr,m,st] = meanAndVar(train_data);
+            a = accuracy(test_data,o,pr,m,st);
+            acc = acc + a;
+        end
+        accAvg = (acc/100);
+        loss = 100 - accAvg;
+        disp("K " + k + " S " + s+ " Accuracy is "+ accAvg+ " loss is " + loss);
+    end
 end
 end
 
 function d = construct(ind, data)
+disp(ind);
+disp(ind');
 d = data(:,[ind',58]);
 end
 
@@ -67,14 +70,14 @@ for k = 1: length(o)
         x = [];  pos = 1;
         for tj = 1: size(trainData,1)
             if trainData(tj,size(trainData,2)) == o(k)
-            x(pos) = trainData(tj,i);
-            pos = pos +1;
-            end    
-        end  
+                x(pos) = trainData(tj,i);
+                pos = pos +1;
+            end
+        end
         m(k,i) = mean(x);
         s(k,i) = std(x);
-    end    
-end    
+    end
+end
 end
 
 function acc = accuracy(testData,o, prior, m,s)
@@ -87,16 +90,16 @@ for it  = 1 : size(testData,1)
             n = prod(n);
             if( isnan(n) == 0)
                 p = p * n;
-            end    
+            end
             if n == 0
                 p = p * 0.00000000000000000000001;
-            end    
-        end    
-       finP(kt) = (prior(kt) * p);
+            end
+        end
+        finP(kt) = (prior(kt) * p);
     end
     [v,ind] = max(finP);
     index(it) = ind;
-end    
+end
 a = sum(index' == testData(:,size(testData,2)) +1);
 acc = a/size(testData,1)* 100;
 end
